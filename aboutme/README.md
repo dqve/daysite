4fbbf140819086e457c59f0a8df25d351a40b0da
oneplus-chris <ayooladavid@gmail.com>
2016-12-28 14:25:48 +0100

    Add files via upload

---
+++ aboutme/README.md
@@ -0,0 +1,6 @@
+# day
+
+
+<img src="images/who.png" alt="" />
+<img src="images/am.png" alt="" />
+<img src="images/I.png" alt="" />
---
+++ aboutme/index.php
@@ -0,0 +1,289 @@
+
+
+
+<!DOCTYPE HTML>
+<!--
+    haywhyde.carrd.co | @oneplus_chris 
+	html5up.net       | @ajlkn
+-->
+<html>
+	<head>
+		<title>Ayoola David</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<section id="header">
+				<header class="major">
+					<h1>Ayoola David </h1>
+                    <p>yeah, I put this page up to serve as a brief description about my self 🙂<br/>
+                    its a tad different from my   <a href="http://haywhyde.carrd.co">former page</a></p>
+                    <p><sub>please note this is not an official website.</sub>
+                    </p>
+				</header>
+				<div class="container">
+					<ul class="actions">
+						<li><a href="#one" class="button special scrolly">Lets Begin</a></li>
+					</ul>
+				</div>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="main special">
+				<div class="container">
+					<span class="image fit primary"><img src="images/pic01.jpg" alt="" /></span>
+					<div class="content">
+						<header class="major">
+							<h2>Who I am</h2>
+						</header>
+						<p>I am a Ayoola Akinola Christian David. Uhmm, you could just call me Christian David. I'm a Nigerian student at <a href="http://lmu.edu.ng">Landmark University, Omu-Aran, Kwara State.</a><br/> I love caffiene and orange juice, People say I am good-humored, I guess thats a good thing.  Above all, I believe I am impeccable the way I am and I pride my self in the things I do.</p>
+					</div>
+					<a href="#two" class="goto-next scrolly">Next</a>
+				</div>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="main special">
+				<div class="container">
+					<span class="image fit primary"><img src="images/pic02.jpg" alt="" /></span>
+					<div class="content">
+						<header class="major">
+							<h2>Stuff I do</h2>
+						</header>
+						<p>Okay I am a poly-doer, well not really. I love to be as advanced as possible in everything I do,it seems like I'm "super man" sometimes, that I can do soo much. Lets just say I am a very passionate guy, who loves what he does. </p>
+						<ul class="icons-grid">
+							<li>
+								<span class="icon major fa-money"></span>
+								<h3>I do business </h3>
+                                <p> I own a food delivery business, a mini academy for teaching students programming and I own a software company (start up) where we code for money.   
+                                </p>
+							</li>
+							<li>
+								<span class="icon major fa-music"></span>
+								<h3>Yes I sing</h3>
+                                <p> This should be one of my strong attributes because I never really had to learn how to sing. I actually don't think I'm that good but people compliment my voice alot so, more for me.    
+                                </p>
+							</li>
+							<li>
+								<span class="icon major fa-code"></span>
+								<h3>I love to code</h3>
+                                <P>Yes I code alot, Its my job, my studies, my work and my hubby. 
+                                </P>
+							</li>
+							<li>
+								<span class="icon major fa-laptop"></span>
+								<h3>I create softwares</h3>
+                                <p>I create softwares in javascript, python, C# etc, for the web, desktops and across all mobile platforms. I even pentest once in a while.  
+                                </p>
+							</li>
+						</ul>
+					</div>
+					<a href="#three" class="goto-next scrolly">Next</a>
+				</div>
+			</section>
+
+		<!-- Three -->
+			<section id="three" class="main special">
+				<div class="container">
+					<span class="image fit primary"><img src="images/pic03.jpg" alt="" /></span>
+					<div class="content">
+						<header class="major">
+							<h2>One more thing</h2>
+						</header>
+						<p>Well all this may fascinate you or maybe not, but this is just a brief description of myself. please if you want to contact me for personal reasons you can do that below. if you wish to contact me regarding my previous projects or regard a job offer also feel free to check out my github page and send me a email. I am always open to new jobs. 😊</p>
+					</div>
+					<a href="#footer" class="goto-next scrolly">Next</a>
+				</div>
+			</section>
+
+
+		<!-- Footer -->
+        
+        
+             <section id="footer">
+				<div class="container">
+					<header class="major">
+						<h2>Get in touch</h2>
+					</header>
+					<form name="contactform" method="post" action="<?php echo $_SERVER["PHP_SELF"]?>">
+						<div class="row uniform">
+							<div class="6u 12u$(xsmall)"><input type="text" name="first_name" id="name" placeholder="Name" maxlength="50" size="30"/></div>
+							<div class="6u$ 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
+							<div class="12u$"><textarea name="comments" id="message" placeholder="Message" rows="4"></textarea></div>
+							<div class="12u$">
+								<ul class="actions">
+                                    <li><input type="submit" value="Send Message" class="special" /></li>
+								</ul>
+							</div>
+						</div>
+					</form>
+				</div>
+				<footer>
+					<ul class="icons">
+						<li><a href="https://twitter.com/oneplus_chris" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="https://www.facebook.com/ayoola.david1" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="https://www.instagram.com/official_dqve/" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="https://github.com/oneplus-chris" class="icon alt fa-github"><span class="label">github</span></a></li>
+						<li><a href="https://www.linkedin.com/in/christian-david-ayoola-761687121?trk=nav_responsive_tab_profile_pic" class="icon alt fa-linkedin"><span class="label">Linkedin</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; HiddenHyve Softwares, inc. </li><li>Design: <a href="http://html5up.net">ajlkn</a> X <a href="http://haywhyde.carrd.co">Christian David</a></li>
+					</ul>
+				</footer>
+			</section>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
+
+<?php
+ 
+if(isset($_POST['email'])) {
+ 
+     
+ 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+ 
+    $email_to = "ayooladavid@outlook.com";
+ 
+    $email_subject = "day.hiddenhyve.com";
+ 
+     
+ 
+     
+ 
+    function died($error) {
+ 
+        // your error code can go here
+ 
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+ 
+        echo "These errors appear below.<br /><br />";
+ 
+        echo $error."<br /><br />";
+ 
+        echo "Please go back and fix these errors.<br /><br />";
+ 
+        die();
+ 
+    }
+ 
+     
+ 
+    // validation expected data exists
+ 
+    if(!isset($_POST['first_name']) ||
+ 
+        !isset($_POST['email']) ||
+ 
+        !isset($_POST['comments'])) {
+ 
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+ 
+    }
+ 
+     
+ 
+    $first_name = $_POST['first_name']; // required
+ 
+    $email_from = $_POST['email']; // required
+ 
+    $comments = $_POST['comments']; // required
+ 
+     
+ 
+    $error_message = "";
+ 
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+ 
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$first_name)) {
+ 
+    $error_message .= 'The Name you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+   if(strlen($comments) < 2) {
+ 
+    $error_message .= 'The message you entered do not appear to be valid.<br />';
+ 
+  }
+ 
+  if(strlen($error_message) > 0) {
+ 
+    died($error_message);
+ 
+  }
+ 
+    $email_message = "Form details below.\n\n";
+ 
+     
+ 
+    function clean_string($string) {
+ 
+      $bad = array("content-type","bcc:","to:","cc:","href");
+ 
+      return str_replace($bad,"",$string);
+ 
+    }
+ 
+     
+ 
+    $email_message .= "Name: ".clean_string($first_name)."\n";
+ 
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+  
+    $email_message .= "message: ".clean_string($comments)."\n";
+ 
+     
+ 
+     
+ 
+// create email headers
+ 
+$headers = 'From: '.$email_from."\r\n".
+ 
+'Reply-To: '.$email_from."\r\n" .
+ 
+'X-Mailer: PHP/' . phpversion();
+ 
+@mail($email_to, $email_subject, $email_message, $headers);  
+ 
+?>
+ 
+ 
+ 
+<!-- include your own success html here -->
+ 
+ 
+ 
+Thank you for contacting us. We will be in touch with you very soon.
+ 
+ 
+ 
+<?php
+ 
+}
+ 
+?>
