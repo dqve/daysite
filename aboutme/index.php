35464280b3d93de60a7b4ebf4a9fc2a47bb32ab9
Ayoola David <ayooladavid@gmail.com>
2016-12-29 13:14:04 +0100

    Update index.php

--- aboutme/index.php
+++ aboutme/index.php
@@ -96,7 +96,11 @@
 						<header class="major">
 							<h2>One more thing</h2>
 						</header>
-						<p>Well all this may fascinate you or maybe not, but this is just a brief description of myself. please if you want to contact me for personal reasons you can do that below. if you wish to contact me regarding my previous projects or regard a job offer also feel free to check out my github page and send me a email. I am always open to new jobs. 😊</p>
+						<p>Well all this may fascinate you or maybe not, but this is just a brief description of myself.
+							please if you want to contact me for personal reasons you can do that below.
+							if you wish to contact me regarding my previous projects or regarding a job offer 
+							also feel free to check out my github page and send me an email.
+							I am always open to new jobs. 😊</p>
 					</div>
 					<a href="#footer" class="goto-next scrolly">Next</a>
 				</div>
@@ -123,6 +127,139 @@
 							</div>
 						</div>
 					</form>
+			<?php
+
+			if(isset($_POST['email'])) {
+
+			    $email_to = "ayooladavid@outlook.com";
+
+			    $email_subject = "day.hiddenhyve.com";
+
+
+
+
+
+			    function died($error) {
+
+				// your error code can go here
+
+				echo "We are very sorry, but there were error(s) found with the message you sent. ";
+
+				echo "These errors appear below.<br /><br />";
+
+				echo $error."<br /><br />";
+
+				echo "Please check again and fix these errors.<br /><br />";
+
+				die();
+
+			    }
+
+
+
+			    // validation expected data exists
+
+			    if(!isset($_POST['first_name']) ||
+
+				!isset($_POST['email']) ||
+
+				!isset($_POST['comments'])) {
+
+				died('We are sorry, but there appears to be a problem with the message you submitted.');       
+
+			    }
+
+
+
+			    $first_name = $_POST['first_name']; // required
+
+			    $email_from = $_POST['email']; // required
+
+			    $comments = $_POST['comments']; // required
+
+
+
+			    $error_message = "";
+
+			    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+			  if(!preg_match($email_exp,$email_from)) {
+
+			    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+
+			  }
+
+			    $string_exp = "/^[A-Za-z .'-]+$/";
+
+			  if(!preg_match($string_exp,$first_name)) {
+
+			    $error_message .= 'The Name you entered does not appear to be valid.<br />';
+
+			  }
+
+			   if(strlen($comments) < 2) {
+
+			    $error_message .= 'The message you entered does not appear to be valid.<br />';
+
+			  }
+
+			  if(strlen($error_message) > 0) {
+
+			    died($error_message);
+
+			  }
+
+			    $email_message = "Form details below.\n\n";
+
+
+
+			    function clean_string($string) {
+
+			      $bad = array("content-type","bcc:","to:","cc:","href");
+
+			      return str_replace($bad,"",$string);
+
+			    }
+
+
+
+			    $email_message .= "Name: ".clean_string($first_name)."\n";
+
+			    $email_message .= "Email: ".clean_string($email_from)."\n";
+
+			    $email_message .= "message: ".clean_string($comments)."\n";
+
+
+
+
+
+			// create email headers
+
+			$headers = 'From: '.$email_from."\r\n".
+
+			'Reply-To: '.$email_from."\r\n" .
+
+			'X-Mailer: PHP/' . phpversion();
+
+			@mail($email_to, $email_subject, $email_message, $headers);  
+
+			?>
+
+
+
+			<!-- include your own success html here -->
+
+
+
+			Thanks for contacting me. I will be in touch with you very soon.
+
+
+
+			<?php
+
+			}
+
+			?>
 				</div>
 				<footer>
 					<ul class="icons">
@@ -150,140 +287,3 @@
 	</body>
 </html>
 
-<?php
- 
-if(isset($_POST['email'])) {
- 
-     
- 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
- 
-    $email_to = "ayooladavid@outlook.com";
- 
-    $email_subject = "day.hiddenhyve.com";
- 
-     
- 
-     
- 
-    function died($error) {
- 
-        // your error code can go here
- 
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
- 
-        echo "These errors appear below.<br /><br />";
- 
-        echo $error."<br /><br />";
- 
-        echo "Please go back and fix these errors.<br /><br />";
- 
-        die();
- 
-    }
- 
-     
- 
-    // validation expected data exists
- 
-    if(!isset($_POST['first_name']) ||
- 
-        !isset($_POST['email']) ||
- 
-        !isset($_POST['comments'])) {
- 
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
- 
-    }
- 
-     
- 
-    $first_name = $_POST['first_name']; // required
- 
-    $email_from = $_POST['email']; // required
- 
-    $comments = $_POST['comments']; // required
- 
-     
- 
-    $error_message = "";
- 
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
- 
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
- 
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$first_name)) {
- 
-    $error_message .= 'The Name you entered does not appear to be valid.<br />';
- 
-  }
- 
-   if(strlen($comments) < 2) {
- 
-    $error_message .= 'The message you entered do not appear to be valid.<br />';
- 
-  }
- 
-  if(strlen($error_message) > 0) {
- 
-    died($error_message);
- 
-  }
- 
-    $email_message = "Form details below.\n\n";
- 
-     
- 
-    function clean_string($string) {
- 
-      $bad = array("content-type","bcc:","to:","cc:","href");
- 
-      return str_replace($bad,"",$string);
- 
-    }
- 
-     
- 
-    $email_message .= "Name: ".clean_string($first_name)."\n";
- 
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-  
-    $email_message .= "message: ".clean_string($comments)."\n";
- 
-     
- 
-     
- 
-// create email headers
- 
-$headers = 'From: '.$email_from."\r\n".
- 
-'Reply-To: '.$email_from."\r\n" .
- 
-'X-Mailer: PHP/' . phpversion();
- 
-@mail($email_to, $email_subject, $email_message, $headers);  
- 
-?>
- 
- 
- 
-<!-- include your own success html here -->
- 
- 
- 
-Thank you for contacting us. We will be in touch with you very soon.
- 
- 
- 
-<?php
- 
-}
- 
-?>
